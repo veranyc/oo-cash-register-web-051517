d86a4267f183386089eb02e896039be4ca5012bd
veranyc <veranyc@gmail.com>
2017-05-29 12:10:50 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,10 +13,16 @@ class CashRegister
   end
 
   def add_item(title, price, quantity=1)
+    #give the quantity a default of 1
+    #take that instance's total and increase it by the price of the item * the
+    #number you are purchasing
     self.total += price * quantity
+    #shove the number of an item purchased into the items array = the cart
     quantity.times do
       items << title
     end
+    #need a variable to keep track of the last transaction to perform a void.
+    #define that here and add to attr_accessor
     self.cart_status = price * quantity
   end
 
